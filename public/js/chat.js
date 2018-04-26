7e40e2b1478a0196e7a44dba5adc297ab87e5287
raghavmalawat <raghavmalawat@gmail.com>
2018-04-26 21:54:24 +0530

    Floating personal messages to the right

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,7 +17,6 @@ function scrollToBottom(){
 
 socket.on('connect',function(){
     var params=jQuery.deparam(window.location.search);
-    
     socket.emit('join',params,function(err){
         if(err){
             alert(err);
@@ -42,6 +41,8 @@ socket.on('updateUserList',function(users){
 });
 
 socket.on('newMessage',function(message){
+    var params=jQuery.deparam(window.location.search);
+
     var col;
     if(message.from=='Admin')
         col='blue';
@@ -49,11 +50,18 @@ socket.on('newMessage',function(message){
         col=message.color;  
     var formattedTime=moment(message.createdAt).format('h:mm a');
     var template=jQuery('#message-template').html();
+    var checkUser="",lineBreak="both";
+    params.name=(params.name).charAt(0).toUpperCase() + (params.name).slice(1).toLowerCase();
+
+    if(params.name===message.from)
+        checkUser="right";
     var html=Mustache.render(template,{
         text:message.text,
         from:message.from,
         createdAt:formattedTime,
-        color:col
+        color:col,
+        checkUser,
+        lineBreak
     });
     jQuery('#messages').append(html); 
     scrollToBottom();
